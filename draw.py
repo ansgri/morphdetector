2690dd8cff7761c4e53d85993543d79a73675db1
Diana <ligferla@rambler.ru>
2014-08-08 19:24:51 +0400

    adding draw.py

--- draw.py
+++ draw.py
@@ -1,3 +1,4 @@
+
 from PyQt4 import QtCore, QtGui
 import sys
 
@@ -5,23 +6,66 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
 
-        #self.resize(250, 150)
+        self.resize(500, 300)
         self.setWindowTitle('Morph-Dete-Ctor')
         menubar = self.menuBar()
         loadImage = menubar.addMenu('&Load image')
 
-        load = QtGui.QAction(QtGui.QIcon('icon.png'), 'Open', self)
+        load = QtGui.QAction(QtGui.QIcon('png.png'), 'Open', self)
         load.setShortcut('Ctrl+O')
         load.setStatusTip('Open image for pattern')
         self.connect(load, QtCore.SIGNAL('triggered()'), self.getImg)
 
         loadImage.addAction(load)
 
+        self.createDockWindows()
+
+    def createDockWindows(self):
+        dock = QtGui.QDockWidget("Group of points", self)
+
+        dock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
+
+        widget = QtGui.QWidget()
+        layout = QtGui.QGridLayout()
+
+        blackLabel = QtGui.QLabel()
+        blackLabel.setText("Black")
+        layout.addWidget(blackLabel , 1, 1)
+
+        blackListWidget = QtGui.QListWidget()
+        blackListWidget.setFlow(0) #left to right
+        for i in range(10):
+            blackListWidget .addItem('( %s , %s )' % (i + 1, i + 2))
+        layout.addWidget(blackListWidget , 1, 2)
+
+        whiteLabel = QtGui.QLabel()
+        whiteLabel.setText("White")
+        layout.addWidget(whiteLabel, 2, 1)
+
+        whiteListWidget = QtGui.QListWidget()
+        whiteListWidget.setFlow(0) #left to right
+        for i in range(10):
+            whiteListWidget.addItem('( %s , %s )' % (i - 1, i - 2))
+        layout.addWidget(whiteListWidget, 2, 2)
+
+        widget.setLayout(layout)
+        dock.setWidget(widget)
+
+
+        #layout.addWidget(self.listWidget, 1)
+        # dock.setWidget(self.listWidget)
+        #dock.setWidget(layout)
+        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, dock)
+
+
+
 
     def getImg(self):
         imgName = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 'C:/')
         self.formWidget = FormWidget(self, imgName)
         self.setCentralWidget(self.formWidget)
+
+
 
 
 class FormWidget(QtGui.QWidget):
@@ -34,16 +78,15 @@ class FormWidget(QtGui.QWidget):
 
         self.label.setScaledContents(True)#changes size of window
 
-
-
         self.layout = QtGui.QHBoxLayout(self)
         self.layout.addWidget(self.label)
 
-
-
-
-
-
+# class FormWidget2(QtGui.QWidget):
+#
+#     def __init__(self, parent):
+#         super(FormWidget2, self).__init__(parent)
+#         self.layout = QtGui.QGridLayout(self)
+#         self.layout.setSpacing(10)
 
 
 
